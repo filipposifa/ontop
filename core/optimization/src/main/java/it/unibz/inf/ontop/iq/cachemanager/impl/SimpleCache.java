769e9487124ef40d309df4369b7ac5eeba5430fe
Filippos Yfantis <filipposifa@gmail.com>
2022-08-18 17:30:05 +0300

    Small fixes and idea for handling hash

--- core/optimization/src/main/java/it/unibz/inf/ontop/iq/cachemanager/impl/SimpleCache.java
+++ core/optimization/src/main/java/it/unibz/inf/ontop/iq/cachemanager/impl/SimpleCache.java
@@ -1,9 +1,14 @@
 package it.unibz.inf.ontop.iq.cachemanager.impl;
 
+import com.google.common.reflect.TypeToken;
 import it.unibz.inf.ontop.iq.cachemanager.Cache;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,11 +17,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 import static java.time.temporal.ChronoUnit.DAYS;
+import com.google.gson.Gson;
 
 public class SimpleCache implements Cache {
     //simple cache, uses h hash set of strings that correspond to dates to denote presence of data in cache
 
-    private HashMap<LocalDate, TreeSet> timeCache;
+    private HashMap<LocalDate, HashSet> timeCache;
     //different structure:
     // private HashMap<Double, TreeSet> xCache
     // private HashMap<Double, TreeSet> yCache
@@ -28,12 +34,34 @@ public class SimpleCache implements Cache {
     private String dcTable;
 
     public SimpleCache(String tablenameFDW, String tablenameCache, int maxSize, Connection con) {
-        this.timeCache = new HashMap<LocalDate, TreeSet>();
+
+        //Initialize Cache Fields
         this.maxSize = maxSize;
         this.currSize = 0;
         this.cacheTable = tablenameCache;
         this.dcTable = tablenameFDW;
         this.con = con;
+
+        /*
+        TODO:
+        - Remove emptying cache if it already exists (DELETE FROM)
+        - Fill hash table with existing data
+        - Dump json of hash table for next run
+        - If there is no json, build hash table based on existing cache
+         */
+        String jsonPath = "../CacheData.json";
+        File jsonFile = new File(jsonPath);
+        if (jsonFile.exists() && !jsonFile.isDirectory()) {
+            try {
+                Gson gson = new Gson();
+                Type hashType = new TypeToken<Map<LocalDate, Set<String>>>(){}.getType();
+                Reader jsonReader = new FileReader(jsonPath);
+                this.timeCache = gson.fromJson(jsonReader, hashType);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else this.timeCache = new HashMap<LocalDate, HashSet>();
 
         System.out.println("SC: In the constructor.");
         try (Statement stmt = this.con.createStatement()) {
@@ -50,13 +78,6 @@ public class SimpleCache implements Cache {
             e.printStackTrace();
         }
         System.out.println("SC: Created cache table in given database.");
-
-        /*
-        TODO:
-        - Remove emptying cache if it already exists (DELETE FROM)
-        - Fill hash table with existing data
-        - Update all corresponding fields
-         */
     }
 
     public boolean manage(String minDate, String maxDate, double minX, double maxX, double minY, double maxY, HashSet<String> variables) {
@@ -90,20 +111,22 @@ public class SimpleCache implements Cache {
 
         /*
         If query cannot be answered by cache in total,
-        - update cache accordingly
-        - return and proceed with usual workflow (FDW)
+        - update cache accordingly, return and proceed with usual workflow (FDW)
+        - if query was fully imported, return and proceed with cache
         */
         if (miss) {
             System.out.println("SC: Updating cache...");
 
             /* Get range for data */
-            long difDate = DAYS.between(minD, maxD);
+            long difDate = DAYS.between(minD, maxD);            //range of dates
             long querySize = difDate * variables.size();        //total query size
+            System.out.println("SC: Range of Dates: " + difDate);
+            System.out.println("SC: Query Size: " + querySize);
 
-            /* If query size is bigger than cache capacity, FOR NOW: delete & update everything */
+            /* Different cases based on query size */
             if (querySize > this.maxSize) {
-                System.out.println("SC: Case 1");
-                //clear everything
+                //FOR NOW: delete & update everything
+                System.out.println("SC: Case 1 - Query Larger than Max Cache Size");
                 this.currSize = 0;
                 this.timeCache.clear();
                 try (Statement stmt = this.con.createStatement()) {
@@ -118,6 +141,8 @@ public class SimpleCache implements Cache {
                 int dateRange = this.maxSize/variables.size();
                 minD = maxD.minusDays(dateRange);
                 minDate = minD.format(formatter);
+                System.out.println("SC: Number of Dates to be imported: " + dateRange);
+                System.out.println("SC: [" + minDate + ", " + maxDate + "]");
 
                 for (String column : variables) {
                     try (Statement stmt = this.con.createStatement()) {
@@ -136,17 +161,15 @@ public class SimpleCache implements Cache {
                     System.out.println("SC: Successfully updated cache for variable: " + column);
 
                     //update current cache size and hash
-                    this.currSize += querySize;
+                    this.currSize = this.maxSize;
                     for (LocalDate tmpD = minD; tmpD.isBefore(maxD); tmpD = tmpD.plusDays(1)) {
-                        TreeSet<String> vars = new TreeSet<String>(variables);
+                        HashSet<String> vars = new HashSet<String>(variables);
                         this.timeCache.put(tmpD, vars);
                     }
                 }
             } else if (querySize < this.maxSize && querySize > this.maxSize - this.currSize) {
                 //FOR NOW: delete & update everything
-                System.out.println("SC: Case 2");
-
-                //clear everything
+                System.out.println("SC: Case 2 - Query Larger than Remaining Cache Size");
                 this.currSize = 0;
                 this.timeCache.clear();
                 try (Statement stmt = this.con.createStatement()) {
@@ -180,13 +203,13 @@ public class SimpleCache implements Cache {
                 }
 
                 //update current cache size and hash
-                this.currSize += querySize;
+                this.currSize = (int) querySize;
                 for (LocalDate tmpD = minD; tmpD.isBefore(maxD); tmpD = tmpD.plusDays(1)) {
-                    TreeSet<String> vars = new TreeSet<String>(variables);
+                    HashSet<String> vars = new HashSet<String>(variables);
                     this.timeCache.put(tmpD, vars);
                 }
             } else {
-                System.out.println("SC: Case 3");
+                System.out.println("SC: Case 3 - Query Smaller than Remaining Cache Size");
                 for (String column : variables) {
                     try (Statement stmt = this.con.createStatement()) {
                         String stmtStr = "ALTER TABLE " + this.cacheTable +
@@ -207,15 +230,26 @@ public class SimpleCache implements Cache {
                 //update current cache size and hash
                 this.currSize += querySize;
                 for (LocalDate tmpD = minD; tmpD.isBefore(maxD); tmpD = tmpD.plusDays(1)) {
-                    TreeSet<String> vars = new TreeSet<String>(variables);
+                    HashSet<String> vars = new HashSet<String>(variables);
                     this.timeCache.put(tmpD, vars);
                 }
+
+                //Exit and proceed with cache
+                System.out.println("SC: Exiting...");
+                return true;
             }
+
+            //Before exiting, print hash set for debug
+            this.timeCache.forEach((key, value) -> System.out.println(key + ": " + value.toString()));
 
             //Exit and proceed with usual workflow (FDW)
             System.out.println("SC: Exiting...");
             return false;
         }
-        else return true;   //Query can be answered by cache
+        else {
+            //Exit and proceed with cache
+            System.out.println("SC: Exiting...");
+            return true;
+        }
     }
 }
